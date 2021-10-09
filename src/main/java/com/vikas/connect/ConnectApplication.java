8bb096fed42b042b5b5c8a5e2027e6d048b686b3
deviknitkkr <deviknitkkr3@gmail.com>
2021-10-09 12:31:02 +0530

    initial commit

---
+++ src/main/java/com/vikas/connect/ConnectApplication.java
@@ -0,0 +1,13 @@
+package com.vikas.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikas/connect/ConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
